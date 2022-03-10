fbaf47803d61ced8e111efc1fa99c942384834f8
Rakib-31 <rakib.hasan6131@gamil.com>
2022-03-11 02:53:08 +0600

    layer of abstraction

---
+++ HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task<T> Get(int id);
+        public Task<IReadOnlyCollection<T>> GetAll();
+        public Task<T> Add(T entity);
+        public Task<T> Update(T entity);
+        public Task<T> Delete(T entity);
+    }
+}
