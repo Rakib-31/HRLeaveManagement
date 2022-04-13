61e204b585ea29bc8de8452b48cfc046f7b1e45b
Rakib-31 <rakib.hasan6131@gamil.com>
2022-04-14 01:32:08 +0600

    add unit test using moq

---
+++ HR.LeaveManagement.Application.UnitTests/LeaveTypes/Command/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Command;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Command;
+using HR.LeaveManagement.Application.Profiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HR.LeaveManagement.Application.UnitTests.LeaveTypes.Command
+{
+    public class CreateLeaveTypeCommandHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        private readonly CreateLeaveTypeDto _leaveTypeDto;
+        private readonly CreateLeaveTypeCommandHandler _handler;
+
+        public CreateLeaveTypeCommandHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetLeaveTypeRepository();
+            var mappingConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfiles>();
+            });
+
+            _mapper = mappingConfig.CreateMapper();
+            _handler = new CreateLeaveTypeCommandHandler(_mockRepo.Object, _mapper);
+
+            _leaveTypeDto = new CreateLeaveTypeDto
+            {
+                DefaultDays = 15,
+                Name = "Test DTO"
+            };
+        }
+
+        [Fact]
+        public async Task ValidLeaveTypeAdded()
+        {
+            var result = await _handler.Handle(new CreateLeaveTypeCommand() { LeaveTypeDto = _leaveTypeDto }, CancellationToken.None);
+            var leaveTypes = await _mockRepo.Object.GetAll();
+            result.ShouldBeOfType<int>();
+            leaveTypes.Count.ShouldBe(3);
+        }
+        public async Task InValidLeaveTypeAdded()
+        {
+            _leaveTypeDto.DefaultDays = -1;
+
+            ValidationException ex = await Should.ThrowAsync<ValidationException>
+                (async () => 
+                    await _handler.Handle(new CreateLeaveTypeCommand() { LeaveTypeDto = _leaveTypeDto }, CancellationToken.None)
+                );
+
+            var leaveTypes = await _mockRepo.Object.GetAll();
+            leaveTypes.Count.ShouldBe(2);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application.UnitTests/LeaveTypes/Queries/GetLeaveTypeListHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using HR.LeaveManagement.Application.Profiles;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Query;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Query;
+using System.Threading;
+using Shouldly;
+using HR.LeaveManagement.Domain.LeaveTypes;
+using HR.LeaveManagement.Application.DTOs;
+
+namespace HR.LeaveManagement.Application.UnitTests.LeaveTypes.Queries
+{
+    public class GetLeaveTypeListHandlerTests
+    {
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        private readonly IMapper _mapper;
+        public GetLeaveTypeListHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetLeaveTypeRepository();
+            var mappingConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfiles>();
+            });
+
+            _mapper = mappingConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypeListRequestHandler(_mockRepo.Object, _mapper);
+            var result = await handler.Handle(new GetLeaveTypeListRequest(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<LeaveTypeDto>>();
+            result.Count.ShouldBe(2);
+        }
+    }
+}
